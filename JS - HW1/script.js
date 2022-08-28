60dc6667928f65cad5b8d400ae34fa0ee5cd9dd2
Uliana Lutchyn <87119160+Ulite10@users.noreply.github.com>
2022-08-28 18:51:11 +0300

    Add files via upload

---
+++ JS - HW1/script.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const firstNum = prompt('Please enter any number', 0);
+const secondNum = prompt('Please enter one more number', 0);
+const result = Number(firstNum) + Number(secondNum)
+const show = alert(String(firstNum) + '+' + String(secondNum) + '=' + result);
