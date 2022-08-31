a7bb165c4c256c2debf95bef3f62a8ccee2ed37f
Uliana <u.lutchyn@gmail.com>
2022-08-31 22:06:49 +0300

    fixed comments

--- HW2/script.js
+++ HW2/script.js
@@ -1,73 +1,44 @@
 'use strict'
 
 let operandA;
+let operandB;
+let operator;
+let result;
 
 do {
     operandA = prompt('OperandA');
 } while (isOperandInvalid(operandA));
 
-function isOperandInvalid(operandA) {
-    if (isNaN(operandA)) {
-        return 'OperandA again';
-    }
-    return operandA === null || operandA === '';
-}
-
-let operandB;
-
 do {
     operandB = prompt('OperandB');
 } while (isOperandInvalid(operandB));
-
-function isOperandInvalid(operandB) {
-    if (isNaN(operandB)) {
-        return 'OperandA again';
-    }
-    return operandB === null || operandB === '';
-}
-
-let operator;
 
 do {
     operator = prompt('Operator');
 } while (isOperatorInvalid(operator));
 
+operandA = +operandA;
+operandB = +operandB;
+
+switch (operator) {
+    case "+":
+    result = operandA + operandB; break;
+    case "-":
+    result = operandA - operandB; break;
+    case "*":
+    result = operandA * operandB; break;
+    case "/":
+    result = operandA / operandB; break
+}
+
+alert(`${operandA} ${operator} ${operandB} = ${result}`)
+
+
+function isOperandInvalid(operand) {
+    return operand === null || operand.trim() === '' || isNaN(operand);
+}
+
 function isOperatorInvalid(operator) {
     return operator !== '+' && operator !== '-' && operator !== '*' && operator !== '/';
 }
 
-operandA = +operandA;
-operandB = +operandB;
-
-
-if (operator === "+") {
-    const result = operandA + operandB;
-    alert(`${operandA} + ${operandB} = ${result}`);
-} else if (operator === "-") {
-    const result = operandA - operandB;
-    alert(`${operandA} - ${operandB} = ${result}`);
-} else if (operator === "*") {
-    const result = operandA * operandB;
-    alert(`${operandA} * ${operandB} = ${result}`);
-} else if (operator === "/") {
-    const result = operandA / operandB;
-    alert(`${operandA} * ${operandB} = ${result}`);
-}
-
-
-
-
-// const resultPlus = operandA + operandB;
-// const resultMinus = operandA - operandB;
-// const resultMultiply = operandA * operandB;
-// const resultDivide = operandA / operandB;
-
-// if (operator === '+') {
-//     alert(`${operandA} + ${operandB} = ${resultPlus}`);
-// } else if (operator === '-') {
-//     alert(`${operandA} - ${operandB} = ${resultMinus}`);
-// } else if (operator === '*') {
-//     alert(`${operandA} * ${operandB} = ${resultMultiply}`);
-// } else if (operator === '/') {
-//     alert(`${operandA} / ${operandB} = ${resultDivide}`);
-// }
